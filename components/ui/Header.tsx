b4593e06a3739e982172e9708b73c3bfcfce7179
Adrian <adrianhenry2115@gmail.com>
2023-11-04 19:49:05 -0400

    header complete

--- components/ui/Header.tsx
+++ components/ui/Header.tsx
@@ -6,6 +6,7 @@ import Link from "next/link";
 
 import { RxHamburgerMenu } from "react-icons/rx";
 import { BiSolidDownArrow } from "react-icons/bi";
+import { AiOutlineClose } from "react-icons/ai";
 
 import Logo from "../../public/assets/icons/brite-logo.png";
 import HeaderLink from "./HeaderLink";
@@ -32,16 +33,106 @@ const Header = () => {
 
     // const LinkClass = "flex items-center hover:bg-blue-400 p-4 rounded-lg transition ease-in duration-300";
     const DropdownClass = "flex flex-col z-20 absolute pt-4 bg-blue-400 rounded-lg p-4 text-sm";
+    const MobileDropdownClass = "flex flex-col w-2/3 items-center my-4 z-20 bg-blue-400 rounded-lg p-4 text-sm hover:bg-white";
 
     return (
-        <nav className="flex bg-white justify-evenly px-10 h-32 lg:h-auto">
+        <nav className="flex bg-white justify-evenly h-32 lg:h-auto">
             <div className="flex items-center lg:justify-evenly justify-between w-full">
                 {/* LOGO  */}
-                <Link className="w-1/6 lg:w-1/6 mr-20" href={"/"}>
-                    <Image alt="logo" src={Logo} />
-                </Link>
-                {/* MENU | MED & SMALL SCREENS*/}
-                <RxHamburgerMenu onClick={() => toggleMenu(true)} className="lg:hidden" size={25} />
+                <div className="w-full flex items-center justify-between p-10">
+                    <Link className="w-1/6 lg:w-1/6 mr-20" href={"/"}>
+                        <Image alt="logo" src={Logo} />
+                    </Link>
+                    {/* MENU | MED & SMALL SCREENS*/}
+                    {!menu && <RxHamburgerMenu onClick={() => toggleMenu(true)} className="lg:hidden cursor-pointer" size={25} />}
+                    {menu && <AiOutlineClose onClick={() => toggleMenu(false)} className="lg:hidden  cursor-pointer" size={25} />}
+                </div>
+                {/* MOBILE DROPDOWN */}
+                {menu && (
+                    <div className="absolute bg-white w-full top-32 flex flex-col items-center z-50 pb-8">
+                        <HeaderLink mobile onClick={() => toggleResidentialOptions(!residential)} name="Residential" isDropdown />
+                        {/* MOBILE RESIDENTIAL DROPDOWN */}
+                        {residential && (
+                            <div
+                                onMouseLeave={() => toggleResidentialOptions(false)}
+                                onMouseEnter={() => toggleResidentialOptions(true)}
+                                className={MobileDropdownClass}
+                            >
+                                {/* RESIDENTIAL DROPDOWN OPTIONS  */}
+                                <Link className="py-4" href={"pro-window-cleaning"}>
+                                    Professional Window Cleaning
+                                </Link>
+                                <Link className="py-4" href={"interior-glass-cleaning"}>
+                                    Interior Glass Cleaning
+                                </Link>
+                                <Link className="py-4" href={"pressure-washing"}>
+                                    Pressure Washing
+                                </Link>
+                                <Link className="py-4" href={"gutter-cleaning"}>
+                                    Gutter Cleaning
+                                </Link>
+                                <Link className="py-4" href={"landscape-lighting"}>
+                                    Landscape Lighting
+                                </Link>
+                                <Link className="py-4" href={"holiday-lighting"}>
+                                    Holiday Lighting
+                                </Link>
+                            </div>
+                        )}
+                        <HeaderLink mobile onClick={() => toggleCommercialOptions(!commercial)} name="Commercial" isDropdown />
+                        {/* MOBILE COMMERCIAL DROPDOWN  */}
+                        {commercial && (
+                            <div
+                                onMouseLeave={() => toggleCommercialOptions(false)}
+                                onMouseEnter={() => toggleCommercialOptions(true)}
+                                className={MobileDropdownClass}
+                            >
+                                {/* RESIDENTIAL DROPDOWN OPTIONS  */}
+                                <Link className="py-4" href={"construction-clean-up"}>
+                                    Construction Clean Up
+                                </Link>
+                                <Link className="py-4" href={"real-estate-cleaning"}>
+                                    Real Estate Cleaning
+                                </Link>
+                                <Link className="py-4" href={"retail-spaces"}>
+                                    Retail Spaces
+                                </Link>
+                                <Link className="py-4" href={"retirement-homes"}>
+                                    Retirement Homes
+                                </Link>
+                            </div>
+                        )}
+                        <HeaderLink mobile onClick={() => toggleAboutOptions(!about)} name="About" isDropdown />
+                        {/* MOBILE ABOUT DROPDOWN  */}
+                        {about && (
+                            <div
+                                onMouseLeave={() => toggleAboutOptions(false)}
+                                onMouseEnter={() => toggleAboutOptions(true)}
+                                className={MobileDropdownClass}
+                            >
+                                {/* RESIDENTIAL DROPDOWN OPTIONS  */}
+                                <Link className="py-4" href={"brand-mission"}>
+                                    Brand Mission & Values
+                                </Link>
+                                <Link className="py-4" href={"giving-back"}>
+                                    Giving Back
+                                </Link>
+                                <Link className="py-4" href={"about"}>
+                                    About Brite
+                                </Link>
+                                <Link className="py-4" href={"guarantee"}>
+                                    Our Guarantee
+                                </Link>
+                                <Link className="py-4" href={"leadership-history"}>
+                                    Leadership & History
+                                </Link>
+                            </div>
+                        )}
+                        <HeaderLink name="Locations" />
+                        <HeaderLink name="Blog" />
+                        <HeaderLink className="bg-blue-500" name="Get Your Free Estimate" />
+                    </div>
+                )}
                 {/* MENU FOR LG SCREENS */}
                 <div className="hidden lg:flex w-full items-center justify-evenly">
                     {/* RESIDENTIAL LINK AND DROPDOWN  */}
@@ -120,7 +211,7 @@ const Header = () => {
                             isDropdown
                             name="About"
                         />
-                        {/* COMMERCIAL DROPDOWN  */}
+                        {/* ABOUT DROPDOWN  */}
                         {about && (
                             <div
                                 onMouseLeave={() => toggleResidentialOptions(false)}
--- components/ui/HeaderLink.tsx
+++ components/ui/HeaderLink.tsx
@@ -5,8 +5,10 @@ import { BiSolidDownArrow } from "react-icons/bi";
 
 interface IHeaderLinkProps {
     name: string;
+    mobile?: boolean;
     onMouseEnter?: () => void;
     onMouseLeave?: () => void;
+    onClick?: () => void;
     className?: string;
     isDropdown?: boolean;
 }
@@ -14,18 +16,26 @@ interface IHeaderLinkProps {
 const HeaderLink = (props: IHeaderLinkProps) => {
     const LinkClass = "flex items-center hover:bg-blue-400 p-4 rounded-lg transition ease-in duration-300";
     const Source = props.name.toLowerCase().replace(" ", "-");
-
-    return (
-        <Link
-            onMouseEnter={props.onMouseEnter}
-            onMouseLeave={props.onMouseLeave}
-            className={`${LinkClass} ${props.className}`}
-            href={Source}
-        >
-            {props.name}
-            {props.isDropdown && <BiSolidDownArrow className="ml-2" size={12} />}
-        </Link>
-    );
+    if (!props.mobile) {
+        return (
+            <Link
+                onMouseEnter={props.onMouseEnter}
+                onMouseLeave={props.onMouseLeave}
+                className={`${LinkClass} ${props.className}`}
+                href={Source}
+            >
+                {props.name}
+                {props.isDropdown && <BiSolidDownArrow className="ml-2" size={12} />}
+            </Link>
+        );
+    } else {
+        return (
+            <span className={`${LinkClass} ${props.className} cursor-pointer`} onClick={props.onClick}>
+                {props.name}
+                {props.isDropdown && <BiSolidDownArrow className="ml-2" size={12} />}
+            </span>
+        );
+    }
 };
 
 export default HeaderLink;
