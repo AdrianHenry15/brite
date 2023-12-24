905217a27c7d58f50dd98a35b19205607ca4f52c
Adrian <adrianhenry2115@gmail.com>
2023-12-24 14:47:21 -0500

    emailjs

--- components/ContactFormContainer.tsx
+++ components/ContactFormContainer.tsx
@@ -1,9 +1,10 @@
 "use client";
 
 import Image from "next/image";
-import React, { use } from "react";
+import React from "react";
 import { useForm } from "react-hook-form";
 import { usePathname } from "next/navigation";
+import emailjs from "@emailjs/browser";
 
 import Logo from "../public/assets/icons/brite-logo.png";
 import HandyMan from "../public/assets/imgs/handyman.webp";
@@ -13,20 +14,49 @@ import Button from "./Button";
 const ContactFormContainer = () => {
     // SWITCH BETWEEN CONTACT AND ESTIMATE FORM | BOTH FORMS DO THE SAME THING FOR NOW
     const pathname = usePathname();
+
     const InputClass = "border-2 border-gray-400 my-2 p-2 rounded-sm w-full shadow-md";
+
+    // EMAIL JS
+    const SERVICE_ID = process.env.SERVICE_ID as string;
+    const TEMPLATE_ID = process.env.TEMPLATE_ID as string;
+    const PUBLIC_KEY = process.env.PUBLIC_KEY as string;
+
     const {
         register,
         handleSubmit,
+        getValues,
         formState: { errors },
     } = useForm();
 
     const onSubmit = (data) => {
+        // EMAIL JS
+        emailjs
+            .send(SERVICE_ID as string, TEMPLATE_ID as string, templateParams, PUBLIC_KEY as string)
+            .then(
+                function (response) {
+                    console.log("SUCCESS!", response.status, response.text);
+                },
+                function (error) {
+                    console.log("FAILED...", error);
+                }
+            );
         console.log(data);
     };
+
+    //EMAIL JS
+    const templateParams = {
+        firstName: getValues("firstName"),
+        lastName: getValues("lastName"),
+        phone: getValues("phone"),
+        email: getValues("email"),
+        comment: getValues("comment"),
+    };
+
     return (
         <section className="flex flex-col items-center px-4 py-20 shadow-inner relative w-full">
             <div className="absolute hidden top-[250px] left-60 2xl:flex">
-                <Image src={HandyMan} alt="Brite Logo" />
+                <Image loading="eager" src={HandyMan} alt="Brite Logo" />
             </div>
             <h1 className="text-3xl mb-10 font-light animate-bounce">{`${
                 pathname === "/contact-us" ? "Contact Us" : "Get Your Free Estimate!"
@@ -35,7 +65,7 @@ const ContactFormContainer = () => {
             <div className="flex flex-col w-[350px] p-6 rounded-2xl shadow-blue-600 shadow-lg border-2 md:w-[650px]">
                 {/* LOGO */}
                 <div className="flex justify-center">
-                    <Image width={100} src={Logo} alt="Brite Logo" />
+                    <Image loading="eager" width={100} src={Logo} alt="Brite Logo" />
                 </div>
                 {/* FORM */}
                 <form className="self-center w-full md:w-2/3" onSubmit={handleSubmit(onSubmit)}>
@@ -64,7 +94,7 @@ const ContactFormContainer = () => {
                         <input
                             className={InputClass}
                             type="tel"
-                            name="phoneNumber"
+                            name="phone"
                             placeholder="Phone Number"
                             {...register("phoneNumber", {
                                 required: false,
@@ -72,7 +102,9 @@ const ContactFormContainer = () => {
                             })}
                         />
                         {errors.phoneNumber && errors.phoneNumber.type === "pattern" && (
-                            <p className="errorMsg">Phone Number should be 10 digits.</p>
+                            <p className="text-sm text-red-600 ml-4">
+                                Phone Number should be 10 digits.
+                            </p>
                         )}
                     </div>
                     <div>
@@ -87,10 +119,10 @@ const ContactFormContainer = () => {
                             })}
                         />
                         {errors.email && errors.email.type === "required" && (
-                            <p className="errorMsg">Email is required.</p>
+                            <p className="text-sm text-red-600 ml-4">Email is required.</p>
                         )}
                         {errors.email && errors.email.type === "pattern" && (
-                            <p className="errorMsg">Email is not valid.</p>
+                            <p className="text-sm text-red-600 ml-4">Email is not valid.</p>
                         )}
                     </div>
                     <div>
--- components/icon-banner/IconBanner.tsx
+++ components/icon-banner/IconBanner.tsx
@@ -1,4 +1,4 @@
-"use client";
+// "use client";
 
 import React, { useEffect, useRef } from "react";
 
@@ -24,35 +24,35 @@ interface IIconBannerProps {
 }
 
 const IconBanner = (props: IIconBannerProps) => {
-    const containerRef = useRef<HTMLDivElement>(null);
+    // const containerRef = useRef<HTMLDivElement>(null);
 
-    useEffect(() => {
-        const options = {
-            threshold: 0.1, // Adjust the threshold as needed (percentage of element visibility)
-        };
+    // useEffect(() => {
+    //     const options = {
+    //         threshold: 0.1, // Adjust the threshold as needed (percentage of element visibility)
+    //     };
 
-        const callback: IntersectionObserverCallback = (entries) => {
-            entries.forEach((entry) => {
-                if (entry.isIntersecting) {
-                    containerRef.current?.classList.add("show");
-                }
-            });
-        };
+    //     const callback: IntersectionObserverCallback = (entries) => {
+    //         entries.forEach((entry) => {
+    //             if (entry.isIntersecting) {
+    //                 containerRef.current?.classList.add("show");
+    //             }
+    //         });
+    //     };
 
-        const observer = new IntersectionObserver(callback, options);
+    //     const observer = new IntersectionObserver(callback, options);
 
-        if (containerRef.current) {
-            observer.observe(containerRef.current);
-        }
+    //     if (containerRef.current) {
+    //         observer.observe(containerRef.current);
+    //     }
 
-        return () => observer.disconnect(); // Cleanup observer on component unmount
-    }, []);
+    //     return () => observer.disconnect(); // Cleanup observer on component unmount
+    // }, []);
     return (
         <section
-            ref={containerRef}
+            // ref={containerRef}
             className={`${
                 props.dark ? "bg-black text-white" : "bg-white text-black"
-            } fade-in w-full justify-evenly text-center py-24 px-4 flex flex-1 flex-col md:flex-row`}
+            } w-full justify-evenly text-center py-24 px-4 flex flex-1 flex-col md:flex-row`}
         >
             <IconItem
                 addBtn={props.addBtn1}
