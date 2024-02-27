b74210908c29832b6464a0d1c6c3cf3d63f20b69
Adrian <adrianhenry2115@gmail.com>
2024-02-27 09:17:30 -0500

    pressure washing description update

--- app/(root)/(routes)/(home)/page.tsx
+++ app/(root)/(routes)/(home)/page.tsx
@@ -20,25 +20,22 @@ export default async function Home() {
                 icon1={<GiWindow size={50} />}
                 link1="/exterior-cleaning"
                 title1={"Window Cleaning"}
-                // TODO: NEEDS UPDATING
                 description1={
                     "Brite's professional window detailing service is Charlotte's first choice for spotless windows year-round"
                 }
                 addBtn1
                 // ITEM 2 - PRESSURE WASHING
-                link2="/exterior/cleaning"
+                link2="/exterior-cleaning"
                 icon2={<IoWaterOutline size={50} />}
                 title2={"Pressure Washing"}
-                // TODO: NEEDS UPDATING
                 description2={
-                    "Brite's professional window detailing service is Charlotte's first choice for spotless windows year-round"
+                    "Brite's professional pressure washing and soft washing services are perfct for maintaining any surface around your home or business."
                 }
                 addBtn2
                 // ITEM 3 - HOLIDAY LIGHTING
                 link3="/holiday-lighting"
                 icon3={<TbChristmasTree size={50} />}
                 title3={"Holiday Lighting"}
-                // TODO: NEEDS UPDATING
                 description3={
                     "Brite's holiday light services are here to provide you with the holiday display of your dreams year after year. We take care of the design, installation, removal and storage of your entire display."
                 }
