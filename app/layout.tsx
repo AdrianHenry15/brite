d083cbe33a8b453cade8bf93668c3cc0806dc99e
Adrian <adrianhenry2115@gmail.com>
2024-02-29 14:36:49 -0500

    stuff

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,8 +24,8 @@ export default async function RootLayout({ children }: { children: React.ReactNo
         <div>
             <link rel="icon" href="/assets/icons/brite-logo.png" />
             <body className={inter.className}>
-                <Analytics />
-                <SpeedInsights />
+                {/* <Analytics />
+                <SpeedInsights /> */}
                 <Toaster />
                 <Script
                     strategy="beforeInteractive"
