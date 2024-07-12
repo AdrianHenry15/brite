ea620fe98355cf8e83975aa64d6a618933d33f96
Adrian <adrianhenry2115@gmail.com>
2024-07-12 10:49:12 -0400

    welcome message deleted

--- app/(root)/(routes)/(home)/page.tsx
+++ app/(root)/(routes)/(home)/page.tsx
@@ -8,7 +8,7 @@ export default async function Home() {
     return (
         <section>
             <HomeSplash />
-            <WelcomeMessage />
+            {/* <WelcomeMessage /> */}
             <SocialProof />
         </section>
     );
