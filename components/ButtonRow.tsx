fa9ce4a0272714bcc159d3813a179a4326d148dd
Adrian <adrianhenry2115@gmail.com>
2024-09-10 12:36:10 -0400

    nav blocks

--- components/ButtonRow.tsx
+++ components/ButtonRow.tsx
@@ -4,10 +4,10 @@ import { NavMenuItems, NavMenuLinks } from "../lib/constants";
 
 const ButtonRow = () => {
     const LinkClass =
-        "hover:scale-110 ease-in-out duration-300 transition-all text-2xl shadow-md border-4 border-t-gray-300 border-l-gray-600 border-r-gray-400 border-black flex items-center justify-center px-6 py-1 text-white bg-blue-500 rounded-md";
+        "hover:scale-110 ease-in-out duration-300 transition-all shadow-md border-4 border-t-gray-300 border-l-gray-600 border-r-gray-400 border-black flex items-center justify-center py-1 text-white bg-blue-500 rounded-md text-[10px] px-2 md:text-sm md:px-6 lg:text-2xl";
 
     return (
-        <div className="flex px-10 py-4 sticky bg-gray-100 items-center justify-evenly w-full h-[10%]">
+        <div className="flex py-4 sticky bg-gray-100 items-center justify-evenly w-full h-[10%] lg:px-10">
             <Link className={LinkClass} href={NavMenuLinks.EXTERIOR_CLEANING}>
                 {NavMenuItems.EXTERIOR_CLEANING}
             </Link>
