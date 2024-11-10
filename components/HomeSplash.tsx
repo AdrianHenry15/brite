c68165deb3991ec938dac5376b16a597e36c2465
Adrian <adrianhenry2115@gmail.com>
2024-11-10 11:26:36 -0500

    some design updates

--- components/HomeSplash.tsx
+++ components/HomeSplash.tsx
@@ -6,59 +6,41 @@ import React from "react";
 
 import ContactFormOverlay from "./contact-form-section/ContactFormOverlay";
 import SplashPic from "../public/assets/imgs/nick-joe-pic.jpg";
+import Logo from "../public/assets/icons/brite-logo.png";
+import Link from "next/link";
 
 const HomeSplash = () => {
     return (
-        <div className="relative w-full text-white lg:h-screen">
+        <div className="relative w-full text-white h-screen">
             <div className="absolute hidden right-[250px] top-[20%] lg:flex lg:top-[15%] lg:right-[350px] xl:right-[450px]">
                 <ContactFormOverlay />
             </div>
             {/* SHADOW */}
-            <div className="z-10 hidden w-full h-screen bg-gradient-to-r from-black lg:flex lg:absolute"></div>
-            <div className="w-full h-[300px] flex justify-center items-center md:h-[400px] lg:h-screen">
+            <div className="z-10 w-full h-screen bg-gradient-to-r from-black flex absolute"></div>
+            <div className="w-full flex justify-center items-center h-screen relative">
                 <Image
                     loading="eager"
                     className="h-full w-full object-cover object-top"
                     src={SplashPic}
                     alt="brite-pic"
                 />
-            </div>
-
-            <div className="z-10 w-full p-4 top-[32%] justify-center lg:top-[10%] lg:absolute">
-                <div className="flex flex-col relative w-full items-center lg:w-[400px]">
-                    {/* ON MEDIUM/LARGE SCREENS */}
-                    {/* <Link className="hidden lg:flex" href={"/"}>
-                        <Image
-                            src={Logo}
-                            alt="logo"
-                            className="cursor-pointer w-24 lg:w-48 rounded-lg"
-                        />
-                    </Link> */}
-                    {/* ON SMALL SCREENS */}
-                    {/* <Link className="flex lg:hidden" href={"/"}>
-                        <Image
-                            src={AltLogo}
-                            alt="alt-logo"
-                            className="cursor-pointer w-24 lg:w-48 rounded-lg"
-                        />
-                    </Link> */}
-                    {/* <div className="flex flex-col items-center w-full lg:ml-0 xl:ml-[400px]">
-                        <SplashBtn
-                            link={NavMenuLinks.EXTERIOR_CLEANING}
-                            title={NavMenuItems.EXTERIOR_CLEANING}
-                            // icon={<IoWater className="w-14 h-full flex lg:w-20" />}
-                        />
-                        <SplashBtn
-                            link={NavMenuLinks.HOLIDAY_LIGHTING}
-                            title={NavMenuItems.HOLIDAY_LIGHTING}
-                            // icon={<CiRuler className="w-14 h-full flex lg:w-20" />}
-                        />
-                        <SplashBtn
-                            link={NavMenuLinks.COMMERCIAL_SERVICES}
-                            title={NavMenuItems.COMMERCIAL_SERVICES}
-                            // icon={<Md1XMobiledata className="w-14 h-full flex lg:w-20" />}
-                        />
-                    </div> */}
+                <div className="absolute flex items-start w-full justify-start flex-col pl-4">
+                    <Image src={Logo} alt="brite-logo" className="object-cover z-10 w-32" />
+                    <h5 className="text-3xl text-white z-10 my-4">Brite Exterior Services</h5>
+                    <div className="w-full z-10 flex items-center whitespace-nowrap">
+                        <Link
+                            href="/contact-us"
+                            className="w-[200px] flex mx-1 items-center justify-center text-sm bg-white text-black z-10 rounded-lg py-1"
+                        >
+                            Contact Us
+                        </Link>
+                        <Link
+                            href="/estimate"
+                            className="w-[200px] mx-1 flex items-center justify-center text-sm bg-black text-white z-10 rounded-lg py-1"
+                        >
+                            Get Your Estimate
+                        </Link>
+                    </div>
                 </div>
             </div>
         </div>
