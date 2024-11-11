3b13b078a0dfc28a2b7ccbfe3131094173e73fc9
Adrian <adrianhenry2115@gmail.com>
2024-11-11 12:30:47 -0500

    banner

--- components/promotional-banner.tsx
+++ components/promotional-banner.tsx
@@ -1,18 +1,18 @@
 import Link from "next/link";
 import React from "react";
 
-async function PromotionalBanner() {
+const PromotionalBanner = () => {
     return (
         <div className="bg-gradient-to-r from-blue-600 via-blue-400 to-blue-600 text-white px-8 py-12 mx-4 mt-4 rounded-xl shadow-2xl transform hover:scale-105 transition duration-500">
             <Link
                 href={"/estimate"}
                 className="container mx-auto flex flex-col items-center text-center"
             >
-                <h2 className="text-2xl sm:text-3xl font-extrabold uppercase tracking-wide mb-6 font-robotoFlex">
+                <h2 className="text-2xl sm:text-3xl font-extrabold uppercase tracking-wide mb-6">
                     Brite Holiday Lighting
                 </h2>
-                <p className="text-md sm:text-xl font-semibold mb-8 max-w-2xl font-robotoFlex">
-                    Let Your Home Shine Bright This Holiday Season! ✨
+                <p className="text-md sm:text-xl font-semibold mb-8 max-w-2xl">
+                    Let Your Home Shine Brite This Holiday Season! ✨
                 </p>
                 <span className="bg-white text-blue-800 py-3 px-8 rounded-full shadow-lg font-bold text-sm transform hover:scale-110 transition duration-300">
                     Get An Estimate!
@@ -20,6 +20,6 @@ async function PromotionalBanner() {
             </Link>
         </div>
     );
-}
+};
 
 export default PromotionalBanner;
